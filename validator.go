93c717fe49932e0431f6693337966179e1f4a4d7
Aloïs Micard <a.micard@vold.lu>
2025-10-27 20:45:24 +0100

    Added basic validator

---
+++ validator.go
@@ -0,0 +1,56 @@
+package validateachangelog
+
+import "fmt"
+
+type Options struct {
+	AllowEmptyVersion       bool
+	AllowMissingReleaseDate bool
+	AllowInvalidChangeType  bool
+}
+
+func Validate(c *Changelog, opts *Options) error {
+	if opts == nil {
+		opts = &Options{}
+	}
+
+	if c == nil {
+		return fmt.Errorf("nil Changelog")
+	}
+
+	if len(c.Versions) == 0 {
+		return fmt.Errorf("no versions found in Changelog")
+	}
+
+	standardChangeTypes := getStandardChangeTypes()
+
+	for _, version := range c.Versions {
+		if version.ReleaseDate.IsZero() && !opts.AllowMissingReleaseDate {
+			return fmt.Errorf("missing release date in Changelog entry %s", version.Version)
+		}
+
+		if len(version.Entries) == 0 && !opts.AllowEmptyVersion {
+			return fmt.Errorf("no entries found in Changelog entry %s", version.Version)
+		}
+
+		if !opts.AllowInvalidChangeType {
+			for changeType, _ := range version.Entries {
+				if _, exists := standardChangeTypes[changeType]; !exists {
+					return fmt.Errorf("invalid change type (%s) in Changelog entry %s", changeType, version.Version)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func getStandardChangeTypes() map[string]interface{} {
+	return map[string]interface{}{
+		"Added":      nil,
+		"Changed":    nil,
+		"Deprecated": nil,
+		"Removed":    nil,
+		"Fixed":      nil,
+		"Security":   nil,
+	}
+}
---
+++ validator_test.go
@@ -0,0 +1,210 @@
+package validateachangelog
+
+import (
+	"testing"
+	"time"
+)
+
+func TestValidateEmptyChangelog(t *testing.T) {
+	c := &Changelog{}
+
+	if err := Validate(c, nil); err == nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogMissingReleaseDateAndAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries:     map[string][]Entry{},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: false,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  true,
+	}); err == nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogMissingReleaseDateAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries:     map[string][]Entry{},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  true,
+	}); err != nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogNonMissingReleaseDateAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Date(2023, 10, 10, 0, 0, 0, 0, time.UTC),
+				Entries:     map[string][]Entry{},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: false,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  true,
+	}); err != nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogEmptyVersionAndAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries:     map[string][]Entry{},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  true,
+	}); err != nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogEmptyVersionAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries:     map[string][]Entry{},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       false,
+		AllowInvalidChangeType:  true,
+	}); err == nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogNonEmptyVersionAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries: map[string][]Entry{
+					"Added": {
+						{Description: "Test description"},
+					},
+				},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       false,
+		AllowInvalidChangeType:  true,
+	}); err != nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogInvalidChangeTypeAndAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries: map[string][]Entry{
+					"InvalidType": {
+						{Description: "Test description"},
+					},
+				},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  true,
+	}); err != nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogInvalidChangeTypeAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries: map[string][]Entry{
+					"InvalidType": {
+						{Description: "Test description"},
+					},
+				},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  false,
+	}); err == nil {
+		t.Fail()
+	}
+}
+
+func TestValidateChangelogValidChangeTypeAndNotAllowed(t *testing.T) {
+	c := &Changelog{
+		Versions: []Version{
+			{
+				Version:     "1.0.0",
+				ReleaseDate: time.Time{},
+				Entries: map[string][]Entry{
+					"Added": {
+						{Description: "Test description"},
+					},
+				},
+			},
+		},
+	}
+
+	if err := Validate(c, &Options{
+		AllowMissingReleaseDate: true,
+		AllowEmptyVersion:       true,
+		AllowInvalidChangeType:  false,
+	}); err != nil {
+		t.Fail()
+	}
+}
